baaf16ecc570ba69be042c79f6552cb06f3d1057
Tanvir <34381989+tshahria@users.noreply.github.com>
2019-04-10 14:18:40 -0400

    filled in README and other minor comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # menu-backend
+
+Use npm install to first install required packages.
+
+Then use "node server.js" to start server on port 8080.
+
+I used a local SQL database instead of an online one because I didn't have access to one. In reality, I wouldn't do this as it is not secure and isn't recommended
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// loads all dependencies
 let express = require('express');
 let app = express();
 
@@ -8,23 +9,27 @@ app.use(bodyParser.json());
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data.db');
 
-// Creating TABLE messages
-
+// Creating TABLE menusection
 let query1 = 'CREATE TABLE IF NOT EXISTS menusection (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) UNIQUE)';
 db.all(query1);
 
+
+// sets path to get all menu sections
 app.get('/menusection', function(request,response) {
     let result = {"menusection": []};
     db.all("SELECT * FROM menusection", function(err, rows) {
         rows.forEach(function (row) {
-            result.menusection.append({
+            result.menusection.push({
                 "id": row.id,
-                "name": row.sectionName
+                "name": row.name
             });
         })
-	});	
+        return response.json(result);
+    });	
+    
 });
 
+//sets path to get a specific menu section by id
 app.get('/menusection/:id', function (request, response) { 
     let id = request.params.id;
     db.get('SELECT * FROM menusection WHERE id=?', [id], function(err,row){
@@ -48,6 +53,8 @@ app.get('/menusection/:id', function (request, response) {
     });
 });
 
+//allows you to put a new item into the database. normally I would use a post request
+// but the instructions given specified put so I used this
 app.put('/menusection', function (request,response){
     let name = request.body.name;
     db.run(`INSERT INTO menusection(name) VALUES(?)`, [name], function(err) {
@@ -75,6 +82,7 @@ app.put('/menusection', function (request,response){
 
 });
 
+//creates a route to change an already existing item using id
 app.post('/menusection/:id', function(request,response){
     let id = request.params.id;
     let name = request.body.name;
@@ -97,6 +105,7 @@ app.post('/menusection/:id', function(request,response){
     return response.json(retObject);
 });
 
+// sets up a route to delete a menusection according to ID
 app.delete('/menusection/:id', function(request,response){
     let id = request.params.id;
     db.run('DELETE FROM menusection WHERE id=?', [id], function(err){
